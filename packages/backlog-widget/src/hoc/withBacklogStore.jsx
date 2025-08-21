65d3a52dd6811b30cf1b99267eb3f1554fe07da4
aswanth9495 <aswanth@scaler.com>
2025-08-22 02:25:34 +0530

    feat: add new hoc file

--- packages/backlog-widget/src/hoc/withBacklogStore.jsx
+++ packages/backlog-widget/src/hoc/withBacklogStore.jsx
@@ -4,7 +4,6 @@ import store from '../store';
 
 function withBacklogStore(Component) {
   return function WithBacklogStore(props) {
-    console.log('withBacklogStore', props);
     return (
       <Provider store={store}>
         <Component {...props} />
